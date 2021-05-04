749845740ba5c557719a7d049e48f954f007deab
xuefei0353 <649934287@qq.com>
2021-05-04 10:54:03 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 # 确保脚本抛出遇到的错误
 set -e
-
+echo "params : $1"
 # 生成静态文件
-npm run docs:build
+# npm run docs:build
 
 # 进入生成的文件夹
-git checkout master
-git status
-git add -A
-git commit -m 'update'
-git push
+# git checkout master
+# git status
+# git add -A
+# git commit -m 'update'
+# git push
