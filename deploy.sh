9f0df21fc5edfa94972398b02a3a7faa72ce070c
xuefei0353 <649934287@qq.com>
2021-05-04 11:01:04 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,4 @@ echo "params : $1"
 cd docs/.vuepress/dist
 git add -A
 git commit -m 'update'
-git push -f git@github.com:xuefei0353/xuefei0353.github.io.git master:gh-pages
+git push git@github.com:xuefei0353/xuefei0353.github.io.git gh-pages
