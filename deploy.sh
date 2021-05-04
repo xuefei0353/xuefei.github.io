6e71cffb728bdfdb9b7bfefdb3b08838a6acb354
xuefei0353 <649934287@qq.com>
2021-05-04 11:11:52 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,8 @@
 
 # 确保脚本抛出遇到的错误
 set -e
-echo "params : $1"
 # 生成静态文件
-# npm run docs:build
-
-# 进入生成的文件夹
-# git checkout master
-# git status
-# git add -A
-# git commit -m 'update'
-# git push
-cd docs/.vuepress/dist
+npm run docs:build
 git add -A
 git commit -m 'update'
-git push git@github.com:xuefei0353/xuefei0353.github.io.git gh-pages
+git push
